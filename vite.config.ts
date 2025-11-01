01d318b668932fc5f8d90e406c7cf5c002f2bb49
胡杨 <>
2025-11-01 20:12:41 +0800

    fix:dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-export default defineConfig((mode) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   const getViteEnv = (target: string): any => env[target];
   return {
-    base: '/yahua/',
     plugins: [vue()],
     server: {
       open: true // 启动后自动打开浏览器
