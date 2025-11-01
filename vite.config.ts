88652ba7455bbd6b1d4abcfb173f76b35c044e8e
胡杨 <>
2025-11-01 19:56:42 +0800

    fix:moren

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-export default defineConfig({
-  base: '/yahua/',
-  plugins: [vue()],
-  server: {
-    open: true // 启动后自动打开浏览器
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig((mode) => {
+  const env = loadEnv(mode, process.cwd());
+  const getViteEnv = (target: string): any => env[target];
+  return {
+    base: '/yahua/',
+    plugins: [vue()],
+    server: {
+      open: true // 启动后自动打开浏览器
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    esbuild: {
+      pure:
+        getViteEnv('VITE_CLEAR_LOG') === '1'
+          ? ['alert', 'console.log', 'console.warn', 'debugger']
+          : []
+    },
+    build: {
+      // outDir: 'dist'
+      // output: {
+      //   chunkFileNames: 'assets/js/yh-[name]-[hash].js',
+      //   entryFileNames: 'assets/js/yh-[name]-[hash].js',
+      //   assetFileNames: 'assets/[ext]/yh-[name]-[hash].[ext]'
+      // }
     }
-  },
-  build: {
-    outDir: 'dist'
-  }
+  };
 });
